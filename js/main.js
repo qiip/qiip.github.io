4b7f738037c1bbcabafa1b55a7447f20b0a16969
qiip <online878@gmail.com>
2016-02-12 10:50:03 +0200

    menu

--- js/main.js
+++ js/main.js
@@ -1,56 +1,4 @@
-/*	Fixed main menu
 
-	================================================== */ 
-
-
-
-	$('.menu').addClass('original').clone().insertAfter('.menu').addClass('cloned').css('position','fixed').css('top','0').css('margin-top','0').css('z-index','500').removeClass('original').hide();
-
-	scrollIntervalID = setInterval(stickIt, 10);
-
-
-
-	function stickIt() {
-
-
-
-	  var orgElementPos = $('.original').offset();
-
-	  orgElementTop = orgElementPos.top;               
-
-
-
-	  if ($(window).scrollTop() >= (orgElementTop)) {
-
-	    // scrolled past the original position; now only show the cloned, sticky element.
-
-
-
-	    // Cloned element should always have same left position and width as original element.     
-
-	    orgElement = $('.original');
-
-	    coordsOrgElement = orgElement.offset();
-
-	    leftOrgElement = coordsOrgElement.left;  
-
-	    widthOrgElement = orgElement.css('width');
-
-	    $('.cloned').css('left',leftOrgElement+'px').css('top',0).css('width',widthOrgElement).show();
-
-	    $('.original').css('visibility','hidden');
-
-	  } else {
-
-	    // not scrolled past the menu; only show the original menu.
-
-	    $('.cloned').hide();
-
-	    $('.original').css('visibility','visible');
-
-	  }
-
-	}
 
 // jQuery for page scrolling feature 
 
@@ -70,7 +18,7 @@
 
 		        $('html,body').animate({
 
-		          scrollTop: target.offset().top - $('.menu.cloned').height()
+		          scrollTop: target.offset().top 
 
 		        }, 1000);
 
@@ -96,7 +44,7 @@ function onScroll(event){
 
         var refElement = $(currLink.attr("href"));
 
-        if (refElement.position().top - $('.menu.cloned').height() <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
 
             $('.menu ul a').removeClass("active");
 

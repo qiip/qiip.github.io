0e7924bb85cc92c2d977c20e323d026b63a7e530
qiip <online878@gmail.com>
2016-02-12 23:06:55 +0200

    404

--- sendemail.php
+++ sendemail.php
@@ -7,16 +7,13 @@ $status = array(
 );
   
     $name = @trim(stripslashes($_POST['name']));
-    $subject = @trim(stripslashes($_POST['subject']));  
-    $email = @trim(stripslashes($_POST['email']));  
     $phone = @trim(stripslashes($_POST['phone']));  
-    $message = @trim(stripslashes($_POST['message']));  
  
     $email_from = $email; 
     $email_to = 'milen-06@mail.ru';//replace with your email
  
  
-    $body = 'Имя: ' . $name . "\n\n" . 'Тип услуги: ' . $subject . "\n\n" . 'Email: ' . $email . "\n\n" . 'Телефон: ' . $phone . "\n\n" . 'Сообщение: ' . $message; 
+    $body = 'Имя: ' . $name . "\n\n" . 'Телефон: ' . $phone; 
  
     $success = @mail($email_to, $phone, $body, 'From: <'.$email_from.'>'); 
  

2a398350926d04ce56cd7c4984d7c61b0e70b715
qiip <online878@gmail.com>
2016-07-02 16:28:08 +0300

    last

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -11,7 +11,7 @@
 var cbpAnimatedHeader = (function() {
 
 	var docElem = document.documentElement,
-		header = document.querySelector( '.masthead' ),
+		header = document.querySelector( '.inner' ),
 		didScroll = false,
 		changeHeaderOn = 100;
 

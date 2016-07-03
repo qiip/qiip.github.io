91a06d749d8ed78328265a84121bfe307e5ea6f0
qiip <online878@gmail.com>
2016-07-03 13:10:55 +0300

    mobile menu

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -42,3 +42,38 @@ var cbpAnimatedHeader = (function() {
 	init();
 
 })();
+
+var cbpAnimatedHeader = (function() {
+
+	var docElem = document.documentElement,
+		header = document.querySelector( '.navbar-brand' ),
+		didScroll = false,
+		changeHeaderOn = 100;
+
+	function init() {
+		window.addEventListener( 'scroll', function( event ) {
+			if( !didScroll ) {
+				didScroll = true;
+				setTimeout( scrollPage, 250 );
+			}
+		}, false );
+	}
+
+	function scrollPage() {
+		var sy = scrollY();
+		if ( sy >= changeHeaderOn ) {
+			classie.add( header, 'navbar-brand-shrink' );
+		}
+		else {
+			classie.remove( header, 'navbar-brand-shrink' );
+		}
+		didScroll = false;
+	}
+
+	function scrollY() {
+		return window.pageYOffset || docElem.scrollTop;
+	}
+
+	init();
+
+})();
--- js/main.js
+++ js/main.js
@@ -47,3 +47,15 @@ function onScroll(event){
         }
     });
 }
+
+// Parallax scroll
+$(document).ready(function(){
+    $('section[data-type="background"]').each(function(){
+        var $bgobj = $(this);
+        $(window).scroll(function() {
+            var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
+            var coords = 'center '+ yPos + 'px';
+            $bgobj.css({ backgroundPosition: coords });
+        });
+    });
+});
